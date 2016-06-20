f551c709e5e082138c1cec43cebe2cc1280dde11
敏誠 李 <LeoLee@minchengde-MacBook-Air.local>
2016-06-20 17:26:25 +0800

    user can post topics

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,5 +1,31 @@
 class TopicsController < ApplicationController
+  before_action :authenticate_user!, :only => [:new, :create, :destroy]
+
   def index
     @topics = Topic.page(params[:page]).per(5)
   end
+
+  def new
+    @topic = Topic.new
+  end
+
+  def create
+    @topic = Topic.new(topic_params)
+    @topic.user = current_user
+
+    if @topic.save
+      redirect_to topics_path
+    else
+      render :new
+    end
+  end
+
+  protected
+
+  def topic_params
+    params.require(:topic).permit(:title, :content)
+  end
+
+
+
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,10 @@
 class Topic < ActiveRecord::Base
   has_many :comments
+  validates_presence_of :title, :content
+
+  belongs_to :user
+
+  def author_name
+    user.try(:display_name) || "Nobody"
+  end
 end
